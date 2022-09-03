70558ed8b60d46d94023cc5f50676d7d44acf6f7
Denis Kyzym <102521716+kyzym@users.noreply.github.com>
2022-09-03 07:37:59 +0300

    first task added

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,32 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+
+let interval;
+
+startBtn.addEventListener('click', changeColorOnBody);
+stopBtn.addEventListener('click', stopChangeColor);
+buttonsGrowPlease();
+
+function buttonsGrowPlease() {
+  let buttons = document.querySelectorAll('button');
+  buttons.forEach(btn => {
+    btn.style.width = '100px';
+    btn.style.height = '60px';
+  });
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function changeColorOnBody() {
+  interval = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startBtn.disabled = true;
+}
+
+function stopChangeColor() {
+  clearInterval(interval);
+  startBtn.disabled = false;
+}
